df28a9f0adb50c34e921d2d0c66b9f5f8c12c7be
Renato Botini do Nascimento <botinirenato@gmail.com>
2021-12-22 14:05:07 -0300

    deploy no heroku

---
+++ src/main/java/com/botidev/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,15 @@
+package com.botidev.apirest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.botidev.apirest.models.Produto;
+
+//Criado Jpa Repository metodos para persistencia no banco de dados
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	Produto findById(long id);
+	
+
+	
+}
---
+++ src/main/java/com/botidev/apirest/resources/ProdutoResource.java
@@ -0,0 +1,79 @@
+package com.botidev.apirest.resources;
+
+import java.util.List;
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.botidev.apirest.models.Produto;
+import com.botidev.apirest.repository.ProdutoRepository;
+
+
+
+
+@RestController
+@RequestMapping(value="/api")
+
+
+
+
+
+public class ProdutoResource {
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+
+	// Metodo Lista Todos os Produtos
+	
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos() {
+		return produtoRepository.findAll();
+	}
+
+	// Metodo listar Produtos passando Parametro id
+	
+	@GetMapping("/produtos/{id}")
+	public Produto listaProdutosUnico(@PathVariable(value = "id") long id) {
+		return produtoRepository.findById(id);
+	}
+
+	// metodo Post Para enviar os dados
+
+	@PostMapping("/produtos")
+	public Produto salvarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	/*
+	 * Metodo Delete Passando Parametro id
+	 * 
+	 * @DeleteMapping("/produtos/{id}") public Produto
+	 * deleteUser(@PathVariable(value = "id")long id) { return
+	 * produtoRepository.deleteById(id); }
+	 */
+	
+	@DeleteMapping("/produtos")
+	public void deletarProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+	
+	
+	@PutMapping("/produtos")
+	public Produto AlterarProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+}
